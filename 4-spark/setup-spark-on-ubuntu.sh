a644f2f61800c93b9a4a0fde71b8599582f31f3c
Brian Amedro <brian@amedro.fr>
2023-09-28 11:38:26 +0200

    Headless apt-get command

--- 4-spark/setup-spark-on-ubuntu.sh
+++ 4-spark/setup-spark-on-ubuntu.sh
@@ -6,8 +6,8 @@
 # - Google Colab (based on Ubuntu 22.04 LTS)
 
 # Spark requires Java 8 or later, but soon, Java version prior to 17 will be deprecated.
-sudo apt-get update -y -qq > /dev/null
-sudo apt-get install openjdk-17-jdk-headless -y -qq > /dev/null
+sudo apt-get update -y -q
+sudo apt-get install openjdk-17-jdk-headless -y -q
 
 # Download Spark. You can check the latest version of Spark at https://spark.apache.org/downloads.html.
 SPARK_VERSION=3.5.0
