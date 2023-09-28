565ad8d9d679e4910be6880928a6828a177f6a64
Brian Amedro <brian@amedro.fr>
2023-09-28 11:11:59 +0200

    Put some comments on setup script

--- 4-spark/setup-spark-on-ubuntu.sh
+++ 4-spark/setup-spark-on-ubuntu.sh
@@ -1,11 +1,16 @@
 #/bin/sh
 
-SPARK_VERSION=3.5.0
+# This script intends to install Spark on Ubuntu 20.04 LTS.
 
+# Spark requires Java 8 or later, but soon, Java version prior to 17 will be deprecated.
 sudo apt update -y -qq > /dev/null
 sudo apt install openjdk-17-jdk-headless -y -qq > /dev/null
+
+# Download Spark. You can check the latest version of Spark at https://spark.apache.org/downloads.html.
+SPARK_VERSION=3.5.0
 wget -q https://dlcdn.apache.org/spark/spark-$SPARK_VERSION/spark-$SPARK_VERSION-bin-hadoop3.tgz
 tar xf spark-$SPARK_VERSION-bin-hadoop3.tgz
 
+# Install findspark and pyspark to use Spark with Python.
 pip install -q findspark
 pip install pyspark==$SPARK_VERSION
